d7a0e1ea1f4e9b6f467b232fe4bb713dfce6824f
mbaldessari-guest <mbaldessari-guest@f06303cf-03cc-411c-aec6-58b7e4ec4d34>
2008-11-24 23:21:36 +0000

    [svn-inject] Applying Debian modifications to trunk
    git-svn-id: svn://svn.debian.org/svn/pkg-fedora-ds/freeipa/trunk@119 f06303cf-03cc-411c-aec6-58b7e4ec4d34

---
+++ debian/freeipa.default.ex
@@ -0,0 +1,10 @@
+# Defaults for freeipa initscript
+# sourced by /etc/init.d/freeipa
+# installed at /etc/default/freeipa by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(freeipa):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="freeipa" command="/usr/bin/freeipa"
